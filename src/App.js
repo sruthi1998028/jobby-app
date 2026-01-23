41f202860225eaf04c9b6f214ed33a2d53d76645
sruthi <sruthibalabhadruni1998@gmail.com>
2026-01-23 09:33:57 +0000

    Fix:Add basename to BrowserRouter for deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {Switch, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
@@ -9,14 +10,16 @@ import JobItemDetails from './components/JobItemDetails'
 import './App.css'
 
 const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/jobs" component={Jobs} />
-    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-    <Route exact path="/not-found" component={NotFound} />
-    <Redirect to="/not-found" />
-  </Switch>
+  <BrowserRouter basename="/jobby-app">
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
