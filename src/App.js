c255b6047aae7fde9fa385d38a191bb2dcee926e
sruthi <sruthibalabhadruni1998@gmail.com>
2026-01-23 09:38:22 +0000

    Fix:Remove duplicate imports and add basename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import {Switch, Route, Redirect} from 'react-router-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
